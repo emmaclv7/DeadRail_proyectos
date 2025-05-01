277837e8cfa4505350f26d8eda84fc94c85114e3
emmaclv7 <emma.clv.07@gmail.com>
2025-05-01 12:04:30 +0100

    Movimiento básico tren

---
+++ Assets/TrainSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TrainSpawner : MonoBehaviour
+{
+    public float growTime = 4f; //lo que tarda en aparecer el tren
+    public float visibleDuration = 90f; //cuánto rato está quieto
+    public Vector3 initialScale = Vector3.zero; //el tamaño inicial del tren
+
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

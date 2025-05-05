3d1dec3fcac2897550a0878518384740cdf9d7d8
HarlockDevv <109815214+HarlockDevv@users.noreply.github.com>
2025-05-05 12:11:39 +0200

    train scale, random train appearence and block/destroy

--- Assets/TrainSpawner.cs
+++ Assets/TrainSpawner.cs
@@ -7,17 +7,48 @@ public class TrainSpawner : MonoBehaviour
     public float growTime = 4f; //lo que tarda en aparecer el tren
     public float maxSize = 6f; //la medida máxima en la que crece el tren
     public bool isMaxSize = false; //si el tren ha llegado al máximo tamaño
+    public Sprite[] trainSprites; // array de sprites para elegir el tren
 
     public float visibleTime = 60f; //tiempo en el que el tren está quieto
     public float maxInterval = 120f; //máximo intervalo en el que el tren puede aparecer
     public float minInterval = 60f; //mínimo intervalo en el que el tren puede aparecer
+
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private BoxCollider2D triggerCollider;
+    [SerializeField] private BoxCollider2D solidCollider;
+    
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //para poder ejecutar el movimiento del tren sin bloquear el juego
         transform.localScale = Vector3.zero;//para asegurar que empieza invisible
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        triggerCollider.enabled = false;
+        solidCollider.enabled = false;
+
         StartCoroutine(TrainAppearece());
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        BoxCollider2D col = GetComponent<BoxCollider2D>();
+        if (col != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(col.offset, col.size);
+        }
     }
     private IEnumerator TrainAppearece()
     {
@@ -29,6 +60,12 @@ public class TrainSpawner : MonoBehaviour
             float waitingTime = Random.Range(minInterval, maxInterval); //genara un numero aleatorio entre los intervalos
             yield return new WaitForSeconds(waitingTime);
 
+            if (trainSprites.Length > 0)
+            {
+                int index = Random.Range(0, trainSprites.Length);
+                spriteRenderer.sprite = trainSprites[index];
+            }
+
             Vector2 initialSize = Vector3.zero; //cuánto mide inicialmente
             Vector2 maximumSize = new Vector3(maxSize, maxSize, maxSize); //cuánto va a crecer
 
@@ -37,12 +74,21 @@ public class TrainSpawner : MonoBehaviour
             {
                 //mientras el timer no llegue al growTime, el tren no para de crecer
                 transform.localScale = Vector3.Lerp(initialSize, maximumSize, timer / growTime);
+
+                if (!triggerCollider.enabled && timer > growTime * 0.75f)
+                {
+                    triggerCollider.enabled = true; // activar colisión
+                    Debug.Log("activado");
+                }
+
                 timer += Time.deltaTime; //incremento timer
                 yield return null;
             }
 
             transform.localScale = maximumSize;
             isMaxSize = true; //ahora ya ha llegado a su máximo tamaño
+            triggerCollider.enabled = false;
+            solidCollider.enabled = true;
 
             yield return new WaitForSeconds(visibleTime); //espera 1 minuto 
 
@@ -51,6 +97,7 @@ public class TrainSpawner : MonoBehaviour
             {
                 //ahora pasa del tamaño máximo a 0
                 transform.localScale = Vector3.Lerp(maximumSize, initialSize, timer / growTime);
+                solidCollider.enabled = false;
                 timer += Time.deltaTime;
                 yield return null;
             }
